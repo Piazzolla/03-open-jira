993c38bf0538aaa17a1e90b57c11652468a09b62
Mariano <macacozzz@hotmail.com>
2022-10-08 10:49:48 -0300

    entries context

---
+++ context/entries/EntriesProvider.tsx
@@ -0,0 +1,51 @@
+import { FC, useReducer } from 'react';
+import { Entry } from '../../interfaces';
+import { EntriesContext, entriesReducer } from './';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface EntriesState {
+   entries: Entry[];
+}
+
+const Entries_INITIAL_STATE: EntriesState = {
+   entries: [
+      {
+         _id: uuidv4(),
+         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius itaque accusamus quidem. Nostrum maxime corporis, quia aliquam, magni veniam soluta incidunt quibusdam asperiores cupiditate, in hic accusantium culpa ipsam.',
+         status: 'pending',
+         createdAt: Date.now()
+      },
+      {
+         _id: uuidv4(),
+         description: 'In sit cupidatat tempor duis proident reprehenderit ad.',
+         status: 'pending',
+         createdAt: Date.now() - 1000000
+      },
+      {
+         _id: uuidv4(),
+         description: 'Commodo eiusmod voluptate commodo velit non esse velit exercitation laborum elit nostrud non.',
+         status: 'pending',
+         createdAt: Date.now() - 100000
+      },
+
+   ],
+}
+
+
+type Props = {
+   children?: React.ReactNode
+};
+
+export const EntriesProvider: FC<Props> = ({ children }) => {
+
+
+   const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
+
+   return (
+      <EntriesContext.Provider value={{
+         ...state,
+      }}>
+         {children}
+      </EntriesContext.Provider>
+   )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
-import Typography from '@mui/material/Typography'
+import { CardHeader, Grid, Card } from '@mui/material'
+import CardContent from '@mui/material/CardContent'
 import type { NextPage } from 'next'
 import { Layout } from '../components/layouts'
 
 const HomePage: NextPage = () => {
   return (
-    <Layout>  
-      <Typography variant='h1' color='primary' >Hola Mundo</Typography>
+    <Layout title='Home - OpenJira'>
+      <Grid container spacing={ 2 }>
+         <Grid item xs={ 12 } sm={ 4 }>
+        {/* sm 4 me da un tercio de la pantalla, xs 12 me da toda la pantalla */}
+          <Card sx={{ height: 'calc(100vh - 100px )'}}>
+            <CardHeader title="Pendientes"/>
+            <CardContent>
+              {/* Agregar una nueva entreada */}
+              {/* listado de entradas */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px )'}}>
+            <CardHeader title="En Progreso"/>
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)'}}>
+            <CardHeader title="Completadas"/>
+          </Card>
+        </Grid>
+
+      </Grid>
     </Layout>
 
   )
